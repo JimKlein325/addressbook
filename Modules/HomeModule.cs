8803e2356fe4e490be74fcfff9bdf98c5ee939e6
Jim Klein <j.mesklein@gmail.com>
2016-06-30 22:21:53 -0700

    fixes to initial compile bugs - added the changes this time.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,8 +1,8 @@
 using Nancy;
 using System.Collections.Generic;
-using CDOrganizer.Objects;
+using AddressBook.Objects;
 
-namespace CDOrganizer
+namespace AddressBook
 {
   public class HomeModule : NancyModule
   {
@@ -11,25 +11,6 @@ namespace CDOrganizer
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
-
-      Get["/cds"] = _ => {
-        // Cd cd1 = new Cd("French Suites","JS Bach",4);
-        // Cd cd2 = new Cd("English Suites","JS Bach",4);
-        var allCds = Cd.GetAllCds();
-        return View["Cds.cshtml", allCds];
-      };
-
-      Get["cds/new"] = _ => {
-        return View["cd_form.cshtml"];
-      };
-
-      Post["/cds"] = _ => {
-        var newCd = new Cd(Request.Form["cd-name"],Request.Form["cd-artist"],Request.Form["cd-price"]);
-        var model = Cd.GetAllCds();
-        return View["Cds.cshtml", model];
-      };
-
-
 
     }
   }
--- Objects/Address.cs
+++ Objects/Address.cs
@@ -1,47 +1,50 @@
-public class Address
+namespace AddressBook.Objects
 {
-	private string _street;
-	private string _city;
-	private string _state;
-	private string _zip;
+  public class Address
+  {
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _zip;
 
-	public Address(string street, string city, string state, string zip)
-	{
-		_street = street;
-		_city = city;
-		_state = state;
-		_zip = zip;
-	}
-	public void SetStreet(string newStreet)
-	{
-		_street = newStreet;
-	}
-	public string GetStreet()
-	{
-		return _street;
-	}
-	public void SetCity(string newcity)
-	{
-		_city = newcity;
-	}
-	public string GetCity()
-	{
-		return this._city;
-	}
-	public void SetState(string newState)
-	{
-		_state = newState;
-	}
-	public string GetState()
-	{
-		return _state;
-	}
-	public void SetZip(string newZip)
-	{
-		_zip = newZip;
-	}
-	public string GetZip()
-	{
-		return _zip;
-	}
+    public Address(string street, string city, string state, string zip)
+    {
+      _street = street;
+      _city = city;
+      _state = state;
+      _zip = zip;
+    }
+    public void SetStreet(string newStreet)
+    {
+      _street = newStreet;
+    }
+    public string GetStreet()
+    {
+      return _street;
+    }
+    public void SetCity(string newcity)
+    {
+      _city = newcity;
+    }
+    public string GetCity()
+    {
+      return this._city;
+    }
+    public void SetState(string newState)
+    {
+      _state = newState;
+    }
+    public string GetState()
+    {
+      return _state;
+    }
+    public void SetZip(string newZip)
+    {
+      _zip = newZip;
+    }
+    public string GetZip()
+    {
+      return _zip;
+    }
+  }
 }
